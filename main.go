337bc5630aba3185ea8b3fc603053a2e4924d6d5
Scythefly <fantasticfarm@hotmail.com>
2020-08-04 17:39:16 +0800

    ..

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	_ "net/http/pprof"
+	// _ "net/http/pprof"
 	"runtime"
 
 	"auxx/command"
@@ -12,7 +11,7 @@ import (
 func main() {
 	runtime.GOMAXPROCS(12)
 
-	go http.ListenAndServe(":6060", nil)
+	// go http.ListenAndServe(":6060", nil)
 
 	if err := command.Execute(); err != nil {
 		fmt.Println(err)
